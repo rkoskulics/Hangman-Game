a3672b3fecc12d237444f3a3823689d98c96680a
Rob <robert.koskulics@me.com>
2017-11-13 13:37:09 -0500

    Getting to the point where I need help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,53 +3,48 @@ var finalAnswers = ["bookkeeper", "bookkeeper"];
 // Randomly choose an item in the array and assign it to a variable
 var toBeGuessed = finalAnswers[Math.floor(Math.random() * finalAnswers.length)];
 // Create a new string with the number of underscores in the variable called emptyGuess
-var emptyGuess = '';
-for (var i = 0; i < toBeGuessed.length; ++i) {
-    emptyGuess += '_';
-}
+
+// var emptyGuess = "";
+// for (var i = 0; i < toBeGuessed.length; ++i) {
+//     emptyGuess.push += "_";
+// }
+for(var guess = ""; guess.length < toBeGuessed.length; guess += "_"){}
+console.log(guess)
 // Create a variable called wins set to 0
 var wins = 0;
 // Create a variable called chances set to 10
 var chances = 10;
-// Create a variable for each of the correct guesses including the dashes
-var newGuess = "";
+
 console.log(toBeGuessed.length);
 // Replace a given underscore with the correct letter when the user types in the correct letter
 document.onkeyup = function (event) {		
 	var letterGuess = event.key;
-	if(toBeGuessed.includes(letterGuess) === false) {
-		chances = chances - 1;
-	}else if (toBeGuessed.includes(letterGuess)) {
-		var replaceDash = toBeGuessed.indexOf(letterGuess);
-		if(replaceDash === 0){
-			newGuess = letterGuess + emptyGuess.substr(0, toBeGuessed.length - 1);
-		}else if(replaceDash === toBeGuessed.length - 1){
-			newGuess = emptyGuess.substr(0, replaceDash) + letterGuess;
-		}else{
-			newGuess = emptyGuess.substr(0, replaceDash) + letterGuess + emptyGuess.substr(replaceDash, toBeGuessed.length - 1);
+	// Adds the user guess to an array named guessArray
+	var guessArray = []
+	guessArray.push(letterGuess);
+		if(toBeGuessed.includes(letterGuess) === false) {
+			chances = chances - 1;
+		}else if (toBeGuessed.includes(letterGuess)) {
+			var replaceDash = toBeGuessed.indexOf(letterGuess);
+			if(replaceDash === 0){
+				guess = letterGuess + guess.substr(0, toBeGuessed.length - 1);
+			}else if(replaceDash === toBeGuessed.length - 1){
+				guess = guess.substr(0, replaceDash) + letterGuess;
+			}else{
+				guess = guess.substr(0, replaceDash) + letterGuess + guess.substr(replaceDash + 1, toBeGuessed.length - 1);
+			}
+			console.log(replaceDash);
+			console.log(guess);
+			console.log(guessArray);
 		}
-		console.log(replaceDash);
-		console.log(newGuess);
-	}
+// Need to show the wins, replaceDash, guess, and guessArray in HTML.
+// Need to figure out how to check for the same letter multiple times.
+	   
 	
-// 	var newGuess = [];
-// 	for(var i=0; i<toBeGuessed.length; i++) {
-//     if (toBeGuessed[i] === letterGuess) {
-//     	newGuess.push(i);
-//     }
-// }
-// }
 
-	// if (toBeGuessed.includes(letterGuess)) {
-	// 	var rightGuess = toBeGuessed.indexOf(letterGuess);
-	// 	console.log(rightGuess);
-	// 		if ((rightGuess = 0) && (rightGuess < toBeGuessed.length())){
-	// 			var replaceDash = toBeGuessed.indexOf(letterGuess);
-	// 			console.log(letterGuess, replaceDash);
-	// 			var newGuess = emptyGuess.substr(replaceDash - 1) + letterGuess;
-	// 			console.log(newGuess);
-	// 		}
 }
+
+
 
 		
 	 
